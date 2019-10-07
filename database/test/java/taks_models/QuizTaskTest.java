2dce93f2b6cf42dff7e81bd219b1569555bc6647
ilya.solomein <ilya.solomein@gmail.com>
2019-10-08 02:16:38 +0300

    tests

---
+++ database/test/java/taks_models/QuizTaskTest.java
@@ -0,0 +1,30 @@
+package taks_models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuizTaskTest {
+
+    @Test
+    public void getQuestion() {
+
+    }
+
+    @Test
+    public void getTaskToString() {
+    }
+
+    @Test
+    public void getRightAnswer() {
+
+    }
+
+    @Test
+    public void checkAnswer() {
+    }
+
+    @Test
+    public void mix() {
+    }
+}
---
+++ database/test/java/tasks_extractor/QuizTasksExtractorTest.java
@@ -0,0 +1,16 @@
+package tasks_extractor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuizTasksExtractorTest {
+
+    @Test
+    public void getRandomTask() {
+    }
+
+    @Test
+    public void getListAllTasks() {
+    }
+}
