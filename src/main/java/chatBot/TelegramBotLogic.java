f6a03210e6161b407ece7a2c8f7bf2ef740aad50
Ilya Solomein <ilya.solomein@gmail.com>
2019-11-19 10:43:25 +0500

    maven structure

---
+++ src/main/java/chatBot/TelegramBotLogic.java
@@ -0,0 +1,46 @@
+package chatBot;
+
+import game.Player;
+import game.QuizGame;
+import game.QuizLogic;
+import publisher_subscriber.ISubscriber;
+import tasks_extractor.QuizTasksExtractor;
+import writers.IWriter;
+import writers.TelegramWriter;
+
+import java.util.HashMap;
+
+public class TelegramBotLogic implements ISubscriber<TelegramMesData> {
+    private QuizTasksExtractor extractor;
+    private final HashMap<String, ISubscriber> subscribers;
+    private TelegramBot telegramBot;
+
+    public TelegramBotLogic(TelegramBot telegramBot, QuizTasksExtractor extractor) {
+        this.telegramBot = telegramBot;
+        subscribers = new HashMap<>();
+        this.extractor = extractor;
+    }
+
+    private ISubscriber createGame(String chatId, String firstName) {
+        IWriter writer = new TelegramWriter(telegramBot, chatId);
+        QuizGame game = new QuizGame(extractor);
+        Player player = new Player(firstName);
+        QuizLogic quizLogic = new QuizLogic(writer, player, game, "src/main/resources/help.txt");
+        quizLogic.startGame();
+        return quizLogic;
+    }
+
+
+    @Override
+    public void objectModified(TelegramMesData data) {
+        synchronized (subscribers) {
+            ISubscriber currentSubscriber = subscribers.get(data.chatId);
+            if (currentSubscriber != null) {
+                currentSubscriber.objectModified(data.text);
+            } else {
+                ISubscriber game = createGame(data.chatId, data.name);
+                subscribers.put(data.chatId, game);
+            }
+        }
+    }
+}
