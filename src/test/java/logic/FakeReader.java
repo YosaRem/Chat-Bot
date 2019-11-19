d7b774448061bf907cd6af047e1774926f53b857
Ilya Solomein <ilya.solomein@gmail.com>
2019-11-19 10:49:00 +0500

    tests

---
+++ src/test/java/logic/FakeReader.java
@@ -0,0 +1,31 @@
+package tests.logic;
+
+import publisher_subscriber.IPublisher;
+import publisher_subscriber.ISubscriber;
+import readers.IReader;
+
+import java.io.IOException;
+
+public class FakeReader implements IReader, IPublisher {
+    private ISubscriber subscriber;
+    private String input = "input";
+
+    @Override
+    public String read() throws IOException {
+        subscriber.objectModified(input);
+        return input;
+    }
+
+    @Override
+    public void subscribe(ISubscriber subscriber) {
+        this.subscriber = subscriber;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public String getInput() {
+        return input;
+    }
+}
