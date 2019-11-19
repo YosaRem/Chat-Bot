83c79b51d3f9d5b22eafefdb1d235aa7fbce42f2
Ilya Solomein <ilya.solomein@gmail.com>
2019-11-19 11:14:21 +0500

    console bot restore

---
+++ src/main/java/consoleBot/ConsoleBot.java
@@ -0,0 +1,27 @@
+package consoleBot;
+
+import game.QuizGame;
+import game.QuizLogic;
+import game.Player;
+import game.PlayerCreator;
+import readers.*;
+import tasks_extractor.QuizTasksExtractor;
+import writers.ConsoleWriter;
+import writers.IWriter;
+
+
+public class ConsoleBot {
+    public static void main(String[] args) {
+        QuizTasksExtractor extractor = new QuizTasksExtractor("src/main/resources/questions");
+        QuizGame game = new QuizGame(extractor);
+        IWriter writer = new ConsoleWriter();
+        ConsoleReader reader = new ConsoleReader(System.in);
+        Player player = new PlayerCreator(reader, writer).cratePlayerFromInput();
+        QuizLogic quizLogic = new QuizLogic(writer, player, game, "src/main/resources/help.txt");
+        reader.subscribe(quizLogic);
+        quizLogic.startGame();
+        while (true) {
+            reader.read();
+        }
+    }
+}
